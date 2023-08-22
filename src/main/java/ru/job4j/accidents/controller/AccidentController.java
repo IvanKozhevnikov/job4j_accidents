dae146ebfdfe12dd6c7680f93adb319107337789
Ivan_Kozhevnikov <IvanKozhevnikof@yandex.ru>
2023-08-22 15:11:57 +0300

    3. @ModelAttribute. Создание инцидента. [#261013]

---
+++ src/main/java/ru/job4j/accidents/controller/AccidentController.java
@@ -0,0 +1,60 @@
+package ru.job4j.accidents.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.job4j.accidents.model.Accident;
+import ru.job4j.accidents.service.AccidentService;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("/accidents") /* Работать с заявками будем по URI /vacancies/** */
+public class AccidentController {
+    private final AccidentService accidentService;
+
+    @GetMapping("/createAccident")
+    public String viewCreateAccident(Model model) {
+        model.addAttribute("accidents", accidentService.findAll());
+        return "accidents/createAccident";
+    }
+
+    @PostMapping("/saveAccident")
+    public String save(@ModelAttribute Accident accident, Model model) {
+        try {
+        accidentService.create(accident);
+        model.addAttribute("message");
+        return "redirect:/index";
+        } catch (Exception exception) {
+            model.addAttribute("message", exception.getMessage());
+            return "errors/404";
+        }
+    }
+
+    @PostMapping("/updateAccident")
+    public String update(@ModelAttribute Accident accident, Model model) {
+        try {
+            var isUpdated = accidentService.update(accident);
+            if (!isUpdated) {
+                model.addAttribute("message", "Заявка с указанным идентификатором не найдена");
+                return "errors/404";
+            }
+            return "redirect:/index";
+        } catch (Exception exception) {
+            model.addAttribute("message", exception.getMessage());
+            return "errors/404";
+        }
+    }
+
+    @GetMapping("/{id}")
+    public String getById(Model model, @PathVariable int id) {
+        var accidentOptional = accidentService.findById(id);
+        if (accidentOptional.isEmpty()) {
+            model.addAttribute("message", "Вакансия с указанным идентификатором не найдена");
+            return "errors/404";
+        }
+        model.addAttribute("accident", accidentOptional.get());
+        return "accidents/updateAccident";
+    }
+}
