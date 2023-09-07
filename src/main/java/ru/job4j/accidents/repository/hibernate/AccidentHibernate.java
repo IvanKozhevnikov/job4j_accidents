1d8069f5967e12a754400cb690ec169217004366
Ivan_Kozhevnikov <IvanKozhevnikof@yandex.ru>
2023-09-07 03:28:54 +0300

    1. Spring ORM [#2093 #442812]

--- src/main/java/ru/job4j/accidents/repository/hibernate/AccidentHibernate.java
+++ src/main/java/ru/job4j/accidents/repository/hibernate/AccidentHibernate.java
@@ -1,85 +1,43 @@
 package ru.job4j.accidents.repository.hibernate;
 
 import lombok.AllArgsConstructor;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 import ru.job4j.accidents.model.Accident;
+import ru.job4j.accidents.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
 @AllArgsConstructor
 public class AccidentHibernate {
-    private final SessionFactory sf;
+
+    private final CrudRepository crudRepository;
 
     public Accident save(Accident accident) {
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
-            session.save(accident);
-            session.getTransaction().commit();
-        } catch (Exception exception) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
+        crudRepository.run(session -> session.save(accident));
         return accident;
     }
 
-    public List<Accident> getAll() {
-        try (Session session = sf.openSession()) {
-            session.beginTransaction();
-            List<Accident> rsl = session.createQuery("from Accident", Accident.class)
-                    .list();
-            session.getTransaction().commit();
-            return rsl;
-        }
-    }
-
     public boolean update(Accident accident) {
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
-            session.merge(accident);
-            session.getTransaction().commit();
-            return true;
-        } catch (Exception exception) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return false;
+        crudRepository.run(session -> session.merge(accident));
+        return true;
     }
 
     public boolean deleteById(int id) {
-        Session session = sf.openSession();
-        try {
-            session.beginTransaction();
-            Accident accident = new Accident();
-            accident.setId(id);
-            session.delete(accident);
-            session.getTransaction().commit();
-            return true;
-        } catch (Exception exception) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return false;
+        Accident accident = new Accident();
+        accident.setId(id);
+        crudRepository.run(session -> session.delete(accident));
+        return true;
+    }
+
+    public List<Accident> getAll() {
+        return crudRepository.query("from Accident", Accident.class);
     }
 
     public Optional<Accident> findById(int id) {
-        try (Session session = sf.openSession()) {
-            session.beginTransaction();
-            Query<Accident> query = session.createQuery(
-                    "from Accident as a where a.id=:fId", Accident.class);
-            query.setParameter("fId", id);
-            Accident rsl = query.uniqueResult();
-            session.getTransaction().commit();
-            return Optional.ofNullable(rsl);
-        }
+        return crudRepository.optional("from Accident as a where a.id=:fId",
+                Accident.class, Map.of("id", id));
     }
 }
