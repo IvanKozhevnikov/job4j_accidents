e91bbd95ea3f638511c36e976c40c28ff407eae2
Ivan_Kozhevnikov <IvanKozhevnikof@yandex.ru>
2023-12-22 01:03:11 +0300

    1. Авторизация через JDBC [#2094]

--- src/main/java/ru/job4j/accidents/service/map/SimpleAccidentService.java
+++ src/main/java/ru/job4j/accidents/service/map/SimpleAccidentService.java
@@ -1,42 +1,42 @@
-//package ru.job4j.accidents.service.map;
-//
-//import lombok.AllArgsConstructor;
-//import ru.job4j.accidents.model.Accident;
-//import ru.job4j.accidents.repository.hibernate.AccidentHibernate;
-//import ru.job4j.accidents.service.impl.AccidentService;
-//
-//import java.util.Collection;
-//import java.util.List;
-//import java.util.Optional;
-//
-//
-//@AllArgsConstructor
-//public class SimpleAccidentService implements AccidentService {
-//
-//    private final AccidentHibernate repository;
-//
-//    @Override
-//    public Accident create(Accident accident) {
-//        return repository.save(accident);
-//    }
-//
-//    @Override
-//    public boolean update(Accident accident) {
-//        return repository.update(accident);
-//    }
-//
-//    @Override
-//    public boolean deleteById(int id) {
-//        return repository.deleteById(id);
-//    }
-//
-//    @Override
-//    public Optional<Accident> findById(int id) {
-//        return repository.findById(id);
-//    }
-//
-//    @Override
-//    public List<Accident> findAll() {
-//        return repository.getAll();
-//    }
-//}
+package ru.job4j.accidents.service.map;
+
+import lombok.AllArgsConstructor;
+import ru.job4j.accidents.model.Accident;
+import ru.job4j.accidents.repository.hibernate.AccidentHibernate;
+import ru.job4j.accidents.service.impl.AccidentService;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+
+@AllArgsConstructor
+public class SimpleAccidentService implements AccidentService {
+
+    private final AccidentHibernate repository;
+
+    @Override
+    public Accident create(Accident accident) {
+        return repository.save(accident);
+    }
+
+    @Override
+    public boolean update(Accident accident) {
+        return repository.update(accident);
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        return repository.deleteById(id);
+    }
+
+    @Override
+    public Optional<Accident> findById(int id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public List<Accident> findAll() {
+        return repository.getAll();
+    }
+}
--- src/main/java/ru/job4j/accidents/service/map/SimpleAccidentTypeService.java
+++ src/main/java/ru/job4j/accidents/service/map/SimpleAccidentTypeService.java
@@ -1,26 +1,26 @@
-//package ru.job4j.accidents.service.map;
-//
-//import lombok.AllArgsConstructor;
-//import ru.job4j.accidents.model.AccidentType;
-//import ru.job4j.accidents.repository.hibernate.AccidentTypeHibernate;
-//import ru.job4j.accidents.service.impl.AccidentTypeService;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//
-//@AllArgsConstructor
-//public class SimpleAccidentTypeService implements AccidentTypeService {
-//
-//    private final AccidentTypeHibernate accidentTypeRepository;
-//
-//    @Override
-//    public List<AccidentType> findAll() {
-//        return accidentTypeRepository.findAll();
-//    }
-//
-//    @Override
-//    public Optional<AccidentType> findById(int id) {
-//        return accidentTypeRepository.findById(id);
-//    }
-//}
+package ru.job4j.accidents.service.map;
+
+import lombok.AllArgsConstructor;
+import ru.job4j.accidents.model.AccidentType;
+import ru.job4j.accidents.repository.hibernate.AccidentTypeHibernate;
+import ru.job4j.accidents.service.impl.AccidentTypeService;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@AllArgsConstructor
+public class SimpleAccidentTypeService implements AccidentTypeService {
+
+    private final AccidentTypeHibernate accidentTypeRepository;
+
+    @Override
+    public List<AccidentType> findAll() {
+        return accidentTypeRepository.findAll();
+    }
+
+    @Override
+    public Optional<AccidentType> findById(int id) {
+        return accidentTypeRepository.findById(id);
+    }
+}
